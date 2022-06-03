2313a24efe641defcf688e4d28d5c2ae1cb341ba
ESLAVATHSANDEEPKUMAR <eslavath.sandeepkumar@kelltontech.com>
2022-06-03 17:21:33 +0530

    sandeep

---
+++ sandeep/program1.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="site.php">
+
+	
+
+	<input type="text" name="username" placeholder="Enter username" required />
+	<br><br>
+	<input type="text" name="password" placeholder="Enter password" required />
+	<br><br>
+	<input type ="submit">
+
+</form>
+
+</body>
+
+
+</html>
