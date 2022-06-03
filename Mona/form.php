27f69771f106d7dc1a14d22db89e6a3bf5ecd97e
MonaAgr <mona.agrawal@kelltontech.com>
2022-06-03 13:55:47 +0530

    Mona First Code

---
+++ Mona/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" conntent="width=device-width, initial-sacle=1">
+    <title>form</title>
+</head>
+<body><center>
+        <form action="register.php" method="post" style="padding-top: 50px;">
+            username: <input type="text" name="name"><br>
+            password: <input type="password" name="pass"><br>
+            <input type="submit" style="color: lightblue;">
+        </form>
+       </center>
+</body>
+</html>
+
