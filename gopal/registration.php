e725d60cf24bce0f17be8c7ee5bab49596189c54
unknown <rudru.krishna@kelltontech.com>
2022-06-05 19:26:07 +0530

    gopal intial code

---
+++ gopal/registration.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<form
+ action="registration"method="post">
+username: <input type="text" name="username"><br>
+password: <input type="text" name="password"><br>
+gender: <input type="radio" name="gender" value="male">male <input type="radio" name="gender" value="female">female<br>
+country:<input type="radio"name="country" value="india">india
+<input type="radio"name="country" value="us">us
+<input type="radio" name="country" value="uk">uk<br>
+date of birth:<input type="text" name="date of birth"><br>
+<button style="background-color:dodgerblue;">submit
+<button>cancel</button>
+</form>
+</body>
