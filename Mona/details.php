b7eb56dfa4cf1e19fa2dbbc500faec29bd080cfc
MonaAgr <mona.agrawal@kelltontech.com>
2022-06-05 12:32:05 +0530

    signform.php

---
+++ Mona/details.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+    <?php echo $_POST["name"]; ?><br>
+    <?php echo $_POST["pass"]; ?><br>
+    <?php echo $_POST["gender"]; ?><br>
+    <?php echo $_POST["country"]; ?><br>
+</body>
+</html>
