be43da4b4da6cb5b629993248db89dfe3e9400d8
ESLAVATHSANDEEPKUMAR <eslavath.sandeepkumar@kelltontech.com>
2022-06-06 10:47:28 +0530

    sandeep

---
+++ sandeep/assignment2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome.php" method="post">
+          USERNAME:<input type="text" name="username"><br>
+          PASSWORD:<input type="password" name="password"><br>
+          GENDER:Male<input type="radio" name="gender:m">
+          Female:<input type="radio" name="gender:f"><br>
+          COUNTRY:UK<input type="radio" name="uk">
+          India:<input type="radio" name="india">
+          Us:<input type="radio" name="us"><br>
+          DATEBOX:<input type="text" name="datebox"><br>
+          <input type="submit" value="submit"><br> 
+          <button>cancel<button>
+
+          
+
+
+</form>    
+</body>
+</html>
