183c1e38bb724db3fd189df08e12a04c9161acf1
Suhas Surwase <suhassurwase1432@gmail.com>
2022-06-06 14:07:16 +0530

    upto conditional/ 6th june

--- Suhas/conditionalStatement.php
+++ Suhas/conditionalStatement.php
@@ -6,5 +6,14 @@ for($i = 1; $i<=10; $i++)
     break;
     echo "num : ".$i."<br>";
 }
+echo "<br><br>";
 
+echo "Name of Technologies<br>";
+$arr = ["1st"=>"JAVA","2nd"=>"PHP","3rd"=>".net"];
+
+foreach ($arr as $key => $value)
+{
+    echo "$key is $value"."<br>";
+    
+}
 ?>
