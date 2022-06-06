16eae371c74976a2a83a938fe9a054b081e902ff
Garide Nanaji <garide.nanaji@kelltontech.com>
2022-06-06 13:00:47 +0530

    Nanaji

---
+++ Nanaji/built.php
@@ -0,0 +1,15 @@
+<?php
+echo sqrt(64); 
+echo "<br>";
+echo abs(-22); 
+echo "<br>";
+echo floor(13.65);
+echo "<br>";
+echo max(12,30,24);
+echo "<br>";
+echo min(12,30,24);
+echo "<br>";
+echo acos(30);
+echo "<br>";
+echo bindec(1011);
+?>
