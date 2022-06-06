884ae46cbbba679bee4fff3ba8e9ca842174fdff
unknown <vavilapalli.kumar@kelltontech.com>
2022-06-06 20:58:07 +0530

    abs.php

---
+++ AJAY/string.php
@@ -0,0 +1,18 @@
+<?php
+echo strtolower("AJAY.");
+echo "<br>";
+echo strrev("ajay");
+echo "<br>";
+echo strtoupper("ajay");
+echo "<br>";
+echo strlen("ajay");
+echo "<br>";
+echo str_repeat("ajay",10);
+echo "<br>";
+print_r(str_split("ajay"));
+echo "<br>";
+echo ucfirst("ajay kumar");
+echo "<br>";
+echo str_word_count("ajay kumar");
+
+?>
