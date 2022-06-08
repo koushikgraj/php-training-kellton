3f4e7279179cf81f439c9a67d4fb3c146218c09b
unknown <rudru.krishna@kelltontech.com>
2022-06-08 08:47:04 +0530

    gopal

---
+++ gopal/myfun.php
@@ -0,0 +1,51 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "data";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM students";
+$result = mysqli_query($conn, $sql);
+?>
+<table border="1">
+	<tr>
+		<th>Name</th>
+		<th>Email</th>
+		<th>&nbsp;</th>
+	</tr>
+<?php
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+?>
+	<tr>
+		<td><?php echo $row['name'];?></td>
+		<td><?php echo $row['email'];?></td>
+		<td>
+			<a href="edit.php?id=<?php echo $row['id'];?>">Edit</a> &nbsp; &nbsp;
+			<a href="delete.php?id=<?php echo $row['id'];?>">Delete</a>
+		</td>
+	</tr>  
+<?php
+  }
+} else {
+  echo "0 results";
+}
+?>
+</table>
+<?php
+
+mysqli_close($conn);
+
+function pr($data)
+{
+	echo "<pre>";print_r($data);echo "</pre>";
+}
+?>
