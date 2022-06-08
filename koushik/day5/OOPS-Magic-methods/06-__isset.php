cc4ec98eb0b388c973117feb10eaf45503439f60
Koushik G <koushikgraj@gmail.com>
2022-06-08 17:03:46 +0530

    OOPS in PHP

---
+++ koushik/day5/OOPS-Magic-methods/06-__isset.php
@@ -0,0 +1,21 @@
+<?php
+class Student {
+    private $data = array();
+ 
+    public function __isset($name) 
+    {
+		echo "calling isset -- ".$name;
+        return isset($this->data[$name]);
+    }
+	
+	public function __unset($name) 
+    {
+		echo "<br>calling unset -- ".$name;
+        return isset($this->data[$name]);
+    }
+}
+ 
+$objStudent = new Student();
+echo isset($objStudent->phone);
+unset($objStudent->phone);
+?>
