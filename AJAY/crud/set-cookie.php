90e2b041b1ee01e838402778bc9157d91b433a6e
unknown <vavilapalli.kumar@kelltontech.com>
2022-06-08 11:33:17 +0530

    crud

---
+++ AJAY/crud/set-cookie.php
@@ -0,0 +1,28 @@
+<?php
+   setcookie("name", "ajay", time()+3600, "/","", 0);
+   setcookie("age", "36", time()+3600, "/", "",  0);
+   setcookie("email", "ajay@gmail.com", time()+3600, "/", "",  0);
+?>
+<?php echo "Set Cookies"?><br>
+<?php
+if( isset($_COOKIE["name"]))
+ echo 'User Name = ', $_COOKIE["name"]. "<br />";
+if( isset($_COOKIE["age"])) 
+ echo 'Age = ', $_COOKIE["age"] . "<br />";
+ 
+if( isset($_COOKIE["email"])){
+	echo "Welcome " . $_COOKIE["email"] . "<br />";
+}
+else{
+	echo "Sorry... Email-ID is Not recognized" . "<br />";
+}
+?>
+<html>
+   
+<head>
+  <title>Setting Cookies with PHP</title>
+</head>
+
+<body>
+  
+</body>
