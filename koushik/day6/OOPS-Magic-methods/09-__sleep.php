9fe9036485d3b5c8b86385d68b211f1bf00fd294
Koushik G <koushikgraj@gmail.com>
2022-06-09 15:19:03 +0530

    OOPS in PHP

---
+++ koushik/day6/OOPS-Magic-methods/09-__sleep.php
@@ -0,0 +1,27 @@
+<?php
+class demoSleepWakeup {
+    public $resourceM;
+    public $arrayM;
+
+    public function __construct() {
+        $this->resourceM = fopen("demo.txt", "w");
+        $this->arrayM = array(1, 2, 3, 4); // Enter code here
+    }
+
+    public function __sleep() {
+		echo "Calling sleep method";
+        return array('arrayM');
+    }
+
+    public function __wakeup() {
+		echo "<br>Calling wakeup method";
+        $this->resourceM = fopen("demo.txt", "w");
+    }
+}
+
+$obj = new demoSleepWakeup();
+$serializedStr = serialize($obj);
+var_dump($obj);
+var_dump($serializedStr);
+var_dump(unserialize($serializedStr));
+?>
