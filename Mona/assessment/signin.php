3e38d34b3209b8e2dddc0d0d414374c8fb002615
MonaAgr <mona.agrawal@kelltontech.com>
2022-06-15 12:09:13 +0530

    Mona

---
+++ Mona/assessment/signin.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <?php echo $_POST["uname"]; ?><br>
+        <?php echo $_POST["pass"]; ?><br>
+        <?php echo $_POST["email"]; ?><br>
+        <?php echo $_POST["date"]; ?><br>
+        <?php echo $_POST["gender"]; ?><br>
+</body>
+</html>
