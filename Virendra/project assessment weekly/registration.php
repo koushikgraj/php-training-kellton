3f9068500c2065266521427f8bfa31cc4e3ccd9a
viren2 <virendra.rajput@kelltontech.com>
2022-06-15 17:54:17 +0530

    assessment weekly test

--- Virendra/project assessment weekly/registration.php
+++ Virendra/project assessment weekly/registration.php
@@ -1,8 +1,6 @@
 <?php
 if(isset($_POST['btnsubmit'])){
-    echo "<pre>";print_r($_POST);echo "</pre>";
-    
-   
+
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
@@ -13,8 +11,10 @@ if(isset($_POST['btnsubmit'])){
 	// Check connection
 	if (!$conn) {
 	  die("Connection failed: " . mysqli_connect_error());
-	}
-	
+	}else{
+        echo "Database connection is Success:<br>";
+      }
+	//connecting to database
 	$user_name = mysqli_real_escape_string($conn, $_POST['user_name']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 	$email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -25,7 +25,9 @@ if(isset($_POST['btnsubmit'])){
 				( user_name, password, email,date,gender)
 				VALUES ('$user_name','$password', '$email','$date','$gender');";
 	$qry = mysqli_query($conn, $sql);
-	  echo "<script>location.replace('list.php?action=add&status=success');</script>";
+
+	
+    
 }
 ?>
 
@@ -43,7 +45,7 @@ if(isset($_POST['btnsubmit'])){
     </div>
 <div id="middle">
     <div style="float:left;" >
-        <h1>Welcome in Singup page</h1>
+        <h1>Welcome in Sing Up page</h1>
  
         <form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
       
@@ -52,7 +54,7 @@ if(isset($_POST['btnsubmit'])){
         <tr><td>Enter Username:</td><td><input type="text" name="user_name" placeholder="Enter Your Username" required=""/></td><br></tr>
         <tr><td>Enter Password:</td><td><input type="password" name="password" placeholder="Enter Your password" required=""/></td></tr>
          <tr><td>Enter Eamil id:</td><td><input type="text" name="email" placeholder="Enter Your Email"  required=""/></td></tr>
-         <tr><td>Date of Birth:</td><td><input type="text" name="date" placeholder="Enter your date of Birth" required=""/></td></tr>
+         <tr><td>Date of Birth:</td><td><input type="date" name="date" placeholder="Enter your date of Birth" required=""/></td></tr>
          <tr><td>Gender:</td<tr><td>
                         <input type= "radio" name="gender"  value='male' required>Male<br>
                         <input type="radio" name="gender"  value='female' required>Famale<br>
@@ -65,34 +67,65 @@ if(isset($_POST['btnsubmit'])){
 
     </div>
 
-
-
 <?php
- $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "kellton";
+if(isset($_POST['submitbtn'])){
+$sn="localhost";
+$un="root";
+$ps="";
+$dn="kellton";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
+$conn=mysqli_connect($sn,$un,$ps,$dn);
+
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+    die("Connection failed: " . mysqli_connect_error());
+  }else{
+    echo "Database connection is Success:<br>";
+  }
 
-$sql = "SELECT * FROM userinfo";
-$result = mysqli_query($conn, $sql);
+  $user_name=$_POST['user_name'];
+  $password=$_POST['password'];
+
+
+//   $query= "SELECT * FROM `db`.`userinfo`";
+
+
+  $sql = "SELECT * FROM `userinfo` WHERE user_name = '$user_name' AND password = '$password' ";
+$result = mysqli_query($conn,$sql); 
+$check = mysqli_fetch_array($result);
+
+//checking the user
+if(isset($check)){
+    echo '"<br><h2>success</h2><br>';
+    }else{
+    echo '"<br><h2>failure</h2><br>"';
+    }
+    echo "<br>";
+echo  $sql;
+echo "<br>";
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="login.php" method="post" style="padding_top: 60px;">
+
+
 
 <div style="float:right;">
     <h1>Welcome in Sing In page</h1>
     
- <form action="index.php" method="post" name='indexlogin'>
+ <form action="login.php" method="post" name='loginphp'>
   
 <table>
     <tr><td>Enter Username</td><td><input type="user_name" name="txtu"  name="user_name" placeholder="Enter your Username" requred=""/></td></tr>
    <tr> <td>Enter password</td><td><input type="password" name="txtp" placeholder="Enter your password" requred=""/></td></tr>
-   <tr><td colspan="4" align ="center" ><input type="submit" name="btnsubmit" value="Login"></td></tr>
+   <tr><td colspan="4" align ="center" ><input type="submit" name="submitbtn" value="Login"></td></tr>
 </table>
  </form>
 </div>
--- Virendra/project assessment weekly/userinfo.sql
+++ Virendra/project assessment weekly/userinfo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2022 at 10:11 AM
+-- Generation Time: Jun 15, 2022 at 02:19 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `userinfo` (
   `user_name` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `date` date NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
   `gender` enum('male','female','','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,13 +40,13 @@ CREATE TABLE `userinfo` (
 --
 
 INSERT INTO `userinfo` (`user_name`, `password`, `email`, `date`, `gender`) VALUES
-('Virendra Rajput', 'vien123', 'virendrarajput041@gm', '0000-00-00', 'male'),
-('shikha', 'shikha123', 'shikha95@gmail.com', '0000-00-00', 'female'),
-('shubham', 'shubham', 'shubham@gmail.com', '0000-00-00', 'male'),
-('', '', '', '0000-00-00', ''),
-('rahul', 'rahul', 'rajputsachin1503@gma', '0000-00-00', 'male'),
-('neha', 'neha', 'neha12@gmail.com', '0000-00-00', 'female'),
-('neha', 'nhea', 'neha12@gmail.com', '0000-00-00', 'female');
+('Virendra Rajput', 'viren123', 'virendrarajput041@gmail.com', '1994-06-21', 'male'),
+('Virendra Rajput', 'viren123', 'virendrarajput041@gmail.com', '1994-06-21', 'male'),
+('shikha', 'shikha123', 'shikha95@gmail.com', '2022-06-03', 'female'),
+('neha', 'neha', 'neha12@gmail.com', '2003-02-06', 'female'),
+('rahul', 'rahulshamr123', 'rahulmewada85@gmail.com', '1995-06-27', 'male'),
+('koushik sir', '#Koushik123', 'koushik85@gmail.com', '1992-01-16', 'male'),
+('narendra ', 'naren#856', 'nareshashwal123@gmail.com', '2003-06-21', 'male');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
