dda8fa14c90d3581e511f0797c3a5c31d8b05ecf
Harsh Singh <harsh.singh@kelltontech.com>
2022-06-20 11:29:29 +0530

    day 9 nd day 10

---
+++ Harsh/Day 9/secondproject/app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'idmember';
+    use HasFactory;
+    protected $fillable = [
+        'email_id','mobile','adhar_number',
+        'title', 'description',
+        'address_line1','address_line2','city',
+        'education','marks'
+    ];
+    
+}
