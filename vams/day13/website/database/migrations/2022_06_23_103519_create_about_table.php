7899657ab3f8887c435a9b3f18e0ec88dbc89f47
unknown <baddam.reddy@kelltontech.com>
2022-06-24 22:20:21 +0530

    day13

---
+++ vams/day13/website/database/migrations/2022_06_23_103519_create_about_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('about', function (Blueprint $table) {
+            $table->id();
+           
+            $table->string('name');
+            $table->string('mothername');
+            $table->string('fathername');
+            $table->string('email');
+            $table->integer('mobile');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about');
+    }
+};
