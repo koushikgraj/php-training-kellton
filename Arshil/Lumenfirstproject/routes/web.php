2e2a0ca6eb736e35137bf29da48067894dac8bc2
Arshil Aziz <arshil.aziz@kelltontech.com>
2022-06-27 22:25:08 +0530

    Lumenfirstproject

---
+++ Arshil/Lumenfirstproject/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+// $router->get('user/{id}', function ($id) {
+//     return 'User '.$id;
+// });
+
+
+// $router->get('g[/{name}]', function ($name = null) {
+//     return $name;
+// });
+// $router->get('type', function () {
+//     return 'Hello World';
+// });    
+// $router->get('profile', ['as' => 'profile', function () {
+//     echo "named routes";
+// }]);
+// $router->get('use/{id}/profile', ['as' => 'profile', function ($id) {
+//     echo "generating urls to named routes";
+// }]);
+
+// $router->get('user/{id}', 'UserController');
+
+// //$url = route('profile', ['id' => 20]);
+
+
+
+
+$router->post (' show/{id}', 'UserController@view' );
