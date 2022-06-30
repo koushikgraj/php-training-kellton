2272c420f7745fcea929749e79f288db54415e3e
Suhas Surwase <suhassurwase1432@gmail.com>
2022-06-30 17:14:42 +0530

    PHPunit test

---
+++ Suhas/Lumen_Exception_Encrypt/app/Exceptions/SampleException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exceptions;
+use Exception;
+
+class SampleException extends Exception {
+
+    public function render()
+    {
+        return view()->make('errors.404Sample');
+    }
+}
